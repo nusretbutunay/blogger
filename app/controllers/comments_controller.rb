fba9531be330eada158296a6b98957180ddc1a13
unknown <nusretbutunay@gmail.com>
2020-07-29 13:38:36 +0300

    complete image upload

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,9 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
-    @comment.save  
+    @comment.save
     redirect_to article_path(@comment.article)
-  end    
+  end
 
   def comment_params
     params.require(:comment).permit(:author_name, :body)
